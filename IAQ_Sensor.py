25a71fbae0326a33031ccdc19e116c3664daf01a
TacoJimmy <t5669016@ntut.org.tw>
2023-11-06 12:25:24 +0800

    Update IAQ_Sensor.py

--- IAQ_Sensor.py
+++ IAQ_Sensor.py
@@ -21,7 +21,7 @@ def Publish_IAQ():
     try:
         client = mqtt.Client()
         client.on_connect
-        client.username_pw_set('fkvRuzah5rKvCb9mZbjG','XXX')
+        client.username_pw_set('TlCpEHdUJUTgiLNovuYO','XXX')
         client.connect('thingsboard.cloud', 1883, 60)
         TimeStamp = Current_ms()
         IAQ_Data = GetIAQ('/dev/ttyS1',1)
